8f21fbe06ccd7e6d7b1a4d615020fa724c685a58
IEyZens <roncint61@gmail.com>
2026-01-16 13:11:55 +0100

    add TypeScript configuration and implement major/minor credits summation functions

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,35 @@
+interface MajorCredits {
+  credits: number;
+  __brand: "MajorCredits";
+}
+
+interface MinorCredits {
+  credits: number;
+  __brand: "MinorCredits";
+}
+
+function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: "MajorCredits",
+  };
+}
+
+function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: "MinorCredits",
+  };
+}
+
+const math = { credits: 20, __brand: "MajorCredits" } as MajorCredits;
+const physics = { credits: 25, __brand: "MajorCredits" } as MajorCredits;
+
+const total = sumMajorCredits(math, physics);
+console.log(total.credits);
